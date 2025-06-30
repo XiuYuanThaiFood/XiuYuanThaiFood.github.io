57f2afae32e0765627714775b09b94a455b179c2
yuki_saito <saito_yuki@trek.co.jp>
2025-06-30 14:09:01 +0900

    動的な要素含む、大まかなレイアウトの実装。teptipの矢印未実装、ほぼすべての画像は未適用

--- js/detail/listup.js
+++ js/detail/listup.js
@@ -1,11 +1,3 @@
-/*
-    <div class="menu-panel"><img src="" width="300px" height="300px">
-		<p>グリッドテスト用</p>
-    </div>
-*/
-//カテゴリごとに別々のscript作る必要はなさそうか
-//foreachの中身を関数化してidと参照する配列を変えつつ全部の種類でやればいいのかもしれない
-
 const RICE_MENU_LIST = [
 	{ imgPath: "./", name: "this panel was added dynamically" },
 	{ imgPath: "./", name: "Seafood Curry Fried Rice" },
@@ -13,22 +5,32 @@ const RICE_MENU_LIST = [
 const NOODLE_MENU_LIST = [
 	{ imgPath: "./", name: "Thai Red Curry Rice Noodles" },
 	{ imgPath: "./", name: "Flat Rice Noodles" },
+	{ imgPath: "./", name: "Flat Rice Noodles", substitute: false },
 ];
 const VEGETABLE_MENU_LIST = [
 	{ imgPath: "./", name: "this panel was added dynamically" },
-	{ imgPath: "./", name: "Seafood Curry Fried Rice" },
+	{ imgPath: "./", name: "vegi1", stirFried: true, shrimp: true },
+	{ imgPath: "./", name: "vegi2", stirFried: true, shrimp: true },
+];
+
+const DRINK_MENU_LIST = [
+	{ imgPath: "./", name: "this panel was added dynamically" },
+	{ imgPath: "./", name: "this panel was added dynamically" },
+	{ imgPath: "./", name: "this panel was added dynamically" },
 ];
 
 const MENU_CLASS = "menu-panel";
 const IMG_SIZE = "300";
+const ICON_SIZE = "50";
 const RICE_DIV_ID = "rice-list";
 const NOODLE_DIV_ID = "noodle-list";
 const VEGETABLE_DIV_ID = "vegitable-list";
-const DRINK_DIV_ID = "drink-list";
+const DRINK_DIV_ID = "drinks-list";
 
 makePanel(RICE_DIV_ID, RICE_MENU_LIST);
 makePanel(NOODLE_DIV_ID, NOODLE_MENU_LIST);
 makePanel(VEGETABLE_DIV_ID, VEGETABLE_MENU_LIST);
+makePanel(DRINK_DIV_ID, DRINK_MENU_LIST);
 
 function makePanel(ID, array) {
 	array.forEach((menu) => {
@@ -38,10 +40,43 @@ function makePanel(ID, array) {
 		img.src = menu.imgPath;
 		img.width = IMG_SIZE;
 		img.height = IMG_SIZE;
-		const p = document.createElement("p");
-		p.textContent = menu.name;
+		const menuName = document.createElement("p");
+		menuName.textContent = menu.name;
 		div.appendChild(img);
-		div.appendChild(p);
+
+		const optDiv = document.createElement("div");
+		optDiv.classList.add("optional-info");
+		//代用不可チェック,キーが存在してかつfalseの場合追加
+		if (menu.substitute === false) {
+			const p = document.createElement("p");
+			p.className = "optional--substituted";
+			p.textContent = "※cannot be substituted.";
+			optDiv.appendChild(p);
+		}
+		if (menu.stirFried === true || menu.shrimp === true) {
+			const iconDiv = document.createElement("div");
+			iconDiv.className = "optional--icons";
+			if (menu.stirFried === true) {
+				const icon = document.createElement("img");
+				icon.width = ICON_SIZE;
+				icon.height = ICON_SIZE;
+				icon.src = "./"; //野菜
+				icon.alt = "野菜";
+				iconDiv.appendChild(icon);
+			}
+			if (menu.shrimp === true) {
+				const icon = document.createElement("img");
+				icon.width = ICON_SIZE;
+				icon.height = ICON_SIZE;
+
+				icon.src = "./"; //エビ
+				icon.alt = "エビ";
+				iconDiv.appendChild(icon);
+			}
+			optDiv.appendChild(iconDiv);
+		}
+		div.appendChild(optDiv);
+		div.appendChild(menuName);
 		document.getElementById(ID).appendChild(div);
 	});
 }
