fd9af6119c1799527411a6c7b838db9c1ae32c6d
yuki_saito <saito_yuki@trek.co.jp>
2025-06-30 15:57:25 +0900

    画像の仮置き。名前確定or新規画像追加で一覧表示に変更がある場合は、listup.js内の配列につくる。

--- js/detail/listup.js
+++ js/detail/listup.js
@@ -1,16 +1,76 @@
 const RICE_MENU_LIST = [
-	{ imgPath: "./", name: "this panel was added dynamically" },
-	{ imgPath: "./", name: "Seafood Curry Fried Rice" },
+	{
+		imgPath: "./image/detail/rice/seafood_curry_fried_rice.webp",
+		name: "Seafood Curry Fried Rice",
+	},
+	{
+		imgPath: "./image/detail/rice/pork_fried_rice_with_egg.webp",
+		name: "Pork Fried Rice with Egg",
+	},
+	{
+		imgPath: "./image/detail/rice/chicken_fried_rice_with_egg.webp",
+		name: "Chicken Fried Rice with Egg",
+	},
+	{
+		imgPath: "./image/detail/rice/shrimp_paste_fried_rice",
+		name: "Shrimp Paste Fried Rice",
+	},
+	{
+		imgPath:
+			"./image/detail/rice/stir-fried_chinese_broccoli_with_chicken.webp",
+		name: "Stir-fried Shinese Broccoli with Chicken",
+	},
+	{
+		imgPath:
+			"./image/detail/rice/stir-fried_cabbage_with_chicken_over_rice.webp",
+		name: "Stir-Fried Cabbage with Chicken over Rice",
+	},
+	{
+		imgPath: "./image/detail/rice/coconut_milk_chicken_over_rice.webp",
+		name: "Coconut Milk Chicken over Rice",
+	},
+	{
+		imgPath: "./image/detail/rice/thai_yellow_chicken_curry_over_rice.webp",
+		name: "Thai Yellow Chicken Curry over Rice",
+	},
+	{
+		imgPath: "./image/detail/rice/hainanese_chicken_rice.webp",
+		name: "Hainanese Chicken Rice (Khao Man Gai)",
+		substitute: false,
+	},
 ];
 const NOODLE_MENU_LIST = [
-	{ imgPath: "./", name: "Thai Red Curry Rice Noodles" },
-	{ imgPath: "./", name: "Flat Rice Noodles" },
-	{ imgPath: "./", name: "Flat Rice Noodles", substitute: false },
+	{
+		imgPath: "./image/detail/noodle/thai_red_curry_rice_noodle.webp",
+		name: "Thai Red Curry Rice Noodles",
+	},
+	{
+		imgPath: "./image/detail/noodle/flat_rice_noodles.webp",
+		name: "Flat Rice Noodles",
+	},
+	{
+		imgPath: "./image/detail/noodle/IMG_1001.webp",
+		name: "Flat Rice Noodles",
+		substitute: false,
+	},
 ];
 const VEGETABLE_MENU_LIST = [
-	{ imgPath: "./", name: "this panel was added dynamically" },
-	{ imgPath: "./", name: "vegi1", stirFried: true, shrimp: true },
-	{ imgPath: "./", name: "vegi2", stirFried: true, shrimp: true },
+	{
+		imgPath: "./image/detail/vegetable/IMG_1021.webp",
+		name: "this panel was added dynamically",
+	},
+	{
+		imgPath: "./image/detail/vegetable/IMG_1019.webp",
+		name: "vegi1",
+		stirFried: true,
+		shrimp: true,
+	},
+	{
+		imgPath: "./image/detail/vegetable/IMG_1020.webp",
+		name: "vegi2",
+		stirFried: true,
+		shrimp: true,
+	},
 ];
 
 const DRINK_MENU_LIST = [
@@ -20,7 +80,7 @@ const DRINK_MENU_LIST = [
 ];
 
 const MENU_CLASS = "menu-panel";
-const IMG_SIZE = "300";
+const IMG_SIZE = "250";
 const ICON_SIZE = "50";
 const RICE_DIV_ID = "rice-list";
 const NOODLE_DIV_ID = "noodle-list";
@@ -41,6 +101,7 @@ function makePanel(ID, array) {
 		img.width = IMG_SIZE;
 		img.height = IMG_SIZE;
 		const menuName = document.createElement("p");
+		menuName.classList.add("menu-name");
 		menuName.textContent = menu.name;
 		div.appendChild(img);
 
