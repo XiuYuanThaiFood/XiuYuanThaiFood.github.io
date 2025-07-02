2558a2d7a9869e4077a75f22dfb1b6b3a8896fe3
yuki_saito <saito_yuki@trek.co.jp>
2025-07-02 12:01:52 +0900

    レスポンシブ仮対応途中。デザイン変更前

--- js/detail/notice-styling.js
+++ js/detail/notice-styling.js
@@ -1,9 +1,17 @@
 //
 
-const noticeDivs = document.querySelectorAll(".customize_notice");
-noticeDivs.forEach((div, l) => {
-	const items = div.querySelectorAll(".opt");
-	items.forEach((el, i) => {
-		el.style.left = `${i * 8}%`;
+function styling() {
+	const noticeDivs = document.querySelectorAll(".customize_notice");
+	const isWide = window.innerWidth > 480;
+	noticeDivs.forEach((div, l) => {
+		const items = div.querySelectorAll(".customize_notice--text");
+		items.forEach((el, i) => {
+			el.style.transform = isWide ? `translateX(${i * 8}vw)` : "none";
+		});
 	});
-});
+}
+
+styling();
+
+// リサイズ対応
+window.addEventListener("resize", styling);
