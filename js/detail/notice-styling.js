aef29e87abad25ab83225247dd9693406e5c0906
yuki_saito <saito_yuki@trek.co.jp>
2025-07-02 16:47:57 +0900

    レスポンシブ対応に関する調整

--- js/detail/notice-styling.js
+++ js/detail/notice-styling.js
@@ -1,17 +1,20 @@
 //
-
+let isBeforeWide;
 function styling() {
-	const noticeDivs = document.querySelectorAll(".customize_notice");
-	const isWide = window.innerWidth > 480;
-	noticeDivs.forEach((div, l) => {
-		const items = div.querySelectorAll(".customize_notice--text");
-		items.forEach((el, i) => {
-			el.style.transform = isWide ? `translateX(${i * 8}vw)` : "none";
-		});
-	});
-}
+	const isWide = window.outerWidth > 480;
+	if (isWide !== isBeforeWide) {
+		isBeforeWide = isWide;
+		const noticeDivs = document.querySelectorAll(".customize_notice");
 
-styling();
+		noticeDivs.forEach((div) => {
+			const items = div.querySelectorAll(".customize_notice--text");
+			items.forEach((el, i) => {
+				el.style.transform = isWide ? `translateX(${i * 8}%)` : "none";
+			});
+		});
+	}
+}
+window.addEventListener("DOMContentLoaded", styling);
 
 // リサイズ対応
 window.addEventListener("resize", styling);
